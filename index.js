810abd8b7556a0f7de26b774f6f3ecdfa3aa21ba
Caleb Holdaway <cdholdaway@gmail.com>
2023-02-25 12:12:03 -0700

    back-end finished

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 const {
     getHouses,
     deleteHouse,
     createHouse,
     updateHouse,
-} = require('./controller')
-app.get(`/api/houses`, getHouses)
-app.post(`/api/houses`, createHouse)
-app.put(`/api/houses/:id`, updateHouse)
-app.delete(`/api/houses/:id`, deleteHouse)
+} = require('./controller');
+
+app.get(`/api/houses`, getHouses);
+app.post(`/api/houses`, createHouse);
+app.put(`/api/houses/:id`, updateHouse);
+app.delete(`/api/houses/:id`, deleteHouse);
 
 app.listen(4004, () => console.log(`running on 4004`))
